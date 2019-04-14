555ceb8d5413cf8972e669d8763e7aaa0ec0b3e0
Liviak5 <livia.kobler@bluewin.ch>
2019-04-14 11:06:15 +0200

    add desc

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Dies ist die offizielle Seite von Autor Rodney Dent">
     <meta http-equiv="Pragma" content="no-cache">
     <title><?php bloginfo('name');?><?php wp_title(' :: ');?></title>
     <?php wp_head();?>
